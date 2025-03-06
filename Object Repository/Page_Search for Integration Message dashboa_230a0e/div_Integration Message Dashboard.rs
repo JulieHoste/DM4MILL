adbc2a7f54525c5259a30141fe270d0cfc148d28
JulieHoste <hostej@delawareconsulting.com>
2025-03-06 17:36:08 +0100

    1425

---
+++ Object Repository/Page_Search for Integration Message dashboa_230a0e/div_Integration Message Dashboard.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Integration Message Dashboard</name>
+   <tag></tag>
+   <elementGuidId>75c52787-b59c-42ff-b66d-0463072bdfa4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@id='__tile209']/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#__tile209 > div.sapMGTHdrContent.OneByOne</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=link[name=&quot;Integration Message Dashboard
+Tile&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>6753f6d4-f3ac-4140-a436-e94efe9ec2d3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>sapMGTHdrContent OneByOne</value>
+      <webElementGuid>43ee85d2-f0eb-488a-8eac-d77c58c25c44</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Integration Message Dashboard</value>
+      <webElementGuid>4cbe6357-429b-4580-8b9c-1b5bc484d5e7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;__tile209&quot;)/div[@class=&quot;sapMGTHdrContent OneByOne&quot;]</value>
+      <webElementGuid>9997c136-6058-4582-99a9-304732e1b1c8</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//a[@id='__tile209']/div</value>
+      <webElementGuid>96fa246e-7259-41bd-b49e-b5d8479a854c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Results (1)'])[1]/following::div[19]</value>
+      <webElementGuid>94d8b1a0-145a-451a-b13e-05e8b8b566d1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Apps'])[1]/following::div[36]</value>
+      <webElementGuid>6f5c3754-550c-40d1-93c4-de0c3ed9eb58</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/div/div/div/a/div</value>
+      <webElementGuid>8539f313-ba3b-40d5-a068-90bcabbc195d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Integration Message Dashboard' or . = 'Integration Message Dashboard')]</value>
+      <webElementGuid>4468a173-0ba6-4d6d-b2c3-ddf107e7f907</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
