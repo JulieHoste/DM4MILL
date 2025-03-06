68c157df451793adaeb19663b9623a4ed329e555
JulieHoste <hostej@delawareconsulting.com>
2025-03-06 14:06:30 +0100

    1

---
+++ Test Suites/TS02 - DM4MILL - GR.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS02 - DM4MILL - GR</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e2272ed7-7085-4603-b94a-79470dd241fc</testSuiteGuid>
+   <testCaseLink>
+      <guid>ef2400a5-8d51-40d3-893f-09dc95779ce7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DM4MILL - Goods Receipts</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
